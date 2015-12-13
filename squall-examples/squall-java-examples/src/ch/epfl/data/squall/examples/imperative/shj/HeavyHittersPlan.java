ea15571f71d03e34c0416af9dc136877b06ad259
Mark Coatsworth <coatsworth@cs.wisc.edu>
2015-12-12 19:08:24 -0600

    Added structure for heavy hitters operator

--- squall-examples/squall-java-examples/src/ch/epfl/data/squall/examples/imperative/shj/HeavyHittersPlan.java
+++ squall-examples/squall-java-examples/src/ch/epfl/data/squall/examples/imperative/shj/HeavyHittersPlan.java
@@ -25,33 +25,33 @@ import ch.epfl.data.squall.components.Component;
 import ch.epfl.data.squall.components.DataSourceComponent;
 import ch.epfl.data.squall.components.EquiJoinComponent;
 import ch.epfl.data.squall.expressions.ColumnReference;
-import ch.epfl.data.squall.operators.AggregateCountOperator;
+import ch.epfl.data.squall.operators.HeavyHittersCountOperator;
 import ch.epfl.data.squall.operators.ProjectOperator;
+import ch.epfl.data.squall.operators.SketchOperator;
 import ch.epfl.data.squall.predicates.ComparisonPredicate;
 import ch.epfl.data.squall.query_plans.QueryPlan;
 import ch.epfl.data.squall.types.IntegerType;
 
-public class HyracksPlan extends QueryPlan {
+public class HeavyHittersPlan extends QueryPlan {
 
-    public HyracksPlan(String dataPath, String extension, Map conf) {
+    public HeavyHittersPlan(String dataPath, String extension, Map conf) {
         super(dataPath, extension, conf);
     }
 
     @Override
     public Component createQueryPlan(String dataPath, String extension, Map conf) {
-        // -------------------------------------------------------------------------------------
-        Component customer = new DataSourceComponent("customer", conf)
-                .add(new ProjectOperator(0, 6));
 
         // -------------------------------------------------------------------------------------
         Component orders = new DataSourceComponent("orders", conf)
                 .add(new ProjectOperator(1));
 
-        // -------------------------------------------------------------------------------------
-        Component custOrders = new EquiJoinComponent(customer, 0, orders, 0)
-                .add(new AggregateCountOperator(conf).setGroupByColumns(1));
+     // -------------------------------------------------------------------------------------
+        Component custOrders = orders
+				.add(new HeavyHittersCountOperator(0, conf).setGroupByColumns(0));
+				//.add(new HeavyHittersAggregateOperator());
 
         return custOrders;
         // -------------------------------------------------------------------------------------
     }
 }
+
